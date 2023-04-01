fbc3d47bcfe29c252bbbb370a3780e6bf1bdfd10
dhanushvarman <dhanushvarmanj@gmail.com>
2023-04-01 11:59:56 +0530

    Final commit

---
+++ src/Data.js
@@ -0,0 +1,287 @@
+export const data = [
+    {
+        id: 1,
+        name: 'James Butt',
+        country: {
+            name: 'Antigua and Barbuda',
+            code: 'AB'
+        },
+        company: 'Benton, John B Jr',
+        date: '2015-09-13',
+        representative:'Ioni Bowcher',
+        balance: 70663
+    }, {
+        id: 2,
+        name: 'Dhanush',
+        country: {
+            name: 'India',
+            code: 'In'
+        },
+        company: 'Guvi',
+        date: '2010-06-13',
+        representative: 'Laura-Beth',
+        balance: 130000
+    }, {
+        id: 3,
+        name: 'Lily-Sue',
+        country: {
+            name: 'United States of America',
+            code: 'USA'
+        },
+        company: 'Accenture',
+        date: '2013-12-13',
+        representative:'Heaven-Leigh',
+        balance: 153502
+    }, {
+        id: 4,
+        name: 'George-Frances',
+        country: {
+            name: 'United Arab Emirates',
+            code: 'UAE'
+        },
+        company: 'Tcs',
+        date: '2022-09-13',
+        representative:'Tucker-Davis',
+        balance: 963000
+    }, {
+        id: 5,
+        name: 'Marley-Blake',
+        country: {
+            name: 'Sri Lanka',
+            code: 'SL'
+        },
+        company: 'Infosys',
+        date: '2016-03-25',
+        representative:'William-Lee',
+        balance: 100000
+    }, {
+        id: 6,
+        name: 'Skyler-Reese',
+        country: {
+            name: 'Singapore',
+            code: 'SG'
+        },
+        company: 'Cognizant',
+        date: '2020-06-01',
+        representative:'Riley-James',
+        balance: 50663
+    },{ 
+        id: 7,
+        name : "Paula Sporer MD", 
+        country: {
+            name:"China",
+            code:"CN"
+        }, 
+        company: "Bauch and Sons", 
+        date: '2018-05-10',
+        representative: "Enrico", 
+        balance: 458800,  
+    },{ 
+        id: 8,
+        name : "Gerardo Rice MD",
+        country: {
+            name : "Iran",
+            code:"Ir"
+        }, 
+        company: "Marquardt - Lehner",
+        date: '2005-06-30', 
+        representative: "Zakary", 
+        balance: 476000,  
+    },{ 
+        id: 9,
+        name : "Darnell Herzog DVM", 
+        country: {
+            name:"Barbados",
+            code:"BD"
+        }, 
+        company: "Dare Inc",
+        date: '2009-06-13', 
+        representative: "Lera", 
+        balance: 477850,  
+    },{ 
+        id: 10,
+        name : "Johnnie Cruickshank DVM", 
+        country: {
+            name:"Guatemala",
+            code:"Gm"
+        }, 
+        company: "Langosh and Sons",
+        date: '2007-06-16',
+        representative: "Vivianne", 
+        balance: 573000,  
+    },{ 
+        id: 11,
+        name : "Mrs. Latoya Abshire", 
+        country: {
+            name:"Jamaica",
+            code:"Ja"
+        }, 
+        company: "Bednar Group", 
+        date: '2003-08-05',
+        representative: "Justen", 
+        balance:776000,  
+    },{ 
+        id: 12,
+        name : "Marianne Gleason Jr.", 
+        country: {
+            name:"Chad",
+            code:"Cd",
+        }, 
+        company: "Rath, Zieme and Strosin",
+        date: '2023-12-13',
+        representative: "Philip", 
+        balance: 748000,  
+    },{ 
+        id: 13,
+        name : "Willis Champlin II", 
+        country: {
+            name:"French Polynesia",
+            code:"FP"
+        }, 
+        company: "Hilll - Walsh",
+        date: '2020-11-12',
+        representative: "Murphy", 
+        balance: 111000, 
+    },{ 
+        id: 14,
+        name : "Stanley Anderson", 
+        country: {
+            name:"Senegal",
+            code:"SG"
+        }, 
+        company: "Orn - Nitzsche",
+        date: '2004-04-20', 
+        representative: "Duane", 
+        balance: 810000,  
+    },{ 
+        id: 15,
+        name : "Kyle Adams" , 
+        country: {
+            name:"Kiribati",
+            code:"KI"
+        }, 
+        company: "Quitzon and Torphy",
+        date: '2006-06-06', 
+        representative: "Adonis", 
+        balance: 198000, 
+    },{ 
+        id: 16,
+        name : "Darin Bauch", 
+        country: {
+            name:"Ecuador",
+            code:"Er"
+        }, 
+        company: "Sanford - Casper",
+        date: '2019-11-31',
+        representative: "Nestor", 
+        balance: 6170000,  
+    },
+    { 
+        id: 17,
+        name : "Kristina Parker", 
+        country: {
+            name:"Cambodia",
+            code:"CA"
+        }, 
+        company: "Halvorson Group",
+        date: '2004-08-12',
+        representative: "Rodger", 
+        balance: 237000,  
+    },
+    { 
+        id: 18,
+        name : "Tommie Bosco", 
+        country: {
+            name:"Burundi",
+            code:"Bi"
+        }, 
+        company: "Williamson - Crist",
+        date: '2002-11-15',
+        representative: "Carlos", 
+        balance: 911000,  
+    },
+    { 
+        id: 19,
+        name : "Allen Murphy", 
+        country: {
+            name:"Malawi",
+            code:"Mi"
+        }, 
+        company: "Bashirian Group",
+        date: '2006-08-01',
+        representative: "Moses", 
+        balance: 957000,  
+    },
+    { 
+        id: 20,
+        name : "Justin Hammes", 
+        country: {
+            name:"Cuba",
+            code:"CA"
+        }, 
+        company: "Cruickshank and Heath",
+        date: '2008-05-16',
+        representative: "Griffin", 
+        balance: 367000,  
+    },
+    { 
+        id: 21,
+        name : "Olga Balistreri", 
+        country: {
+            name:"Qatar",
+            code:"QR"
+        }, 
+        company: "Johns - Morissette",
+        date: '2013-09-20',
+        representative: "Myriam", 
+        balance: 319000,  
+    },
+    { 
+        id: 22,
+        name : "Earnest McKenzie II", 
+        country: {
+            name:"Bermuda",
+            code:"BA"
+        }, 
+        company: "Waters and Schroeder",
+        date: '2018-08-18',
+        representative: "Shannon", 
+        balance: 673000,  
+    },
+    { 
+        id: 23,
+        name : "Don Herman", 
+        country: {
+            name:"Iraq",
+            code:"IQ"
+        }, 
+        company: "Cruickshank - Friesen",
+        date: '2017-12-19',
+        representative: "Carlos", 
+        balance: 790000,  
+    },
+    { 
+        id: 24,
+        name : "Dixie Schroeder", 
+        country: {
+            name:"Gambia",
+            code:"GA"
+        }, 
+        company: "Braun and Sons",
+        date: '2011-11-20',
+        representative: "Talon", 
+        balance: 692000,  
+    },
+    { 
+        id: 25,
+        name : "Kristy Braun", 
+        country: {
+            name:"Bouvet Island",
+            code:"BI"
+        }, 
+        company: "Ortiz LLC",
+        date: '2018-07-25',
+        representative: "Eladio", 
+        balance: 255000,  
+    }
+]
---
+++ src/Screen.js
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from 'react';
+import { FilterMatchMode } from 'primereact/api';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
+import { data } from './Data';
+
+function Screen({value}) {
+
+
+    const [customers, setCustomers] = useState(null);
+    const [filters, setFilters] = useState({
+        global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+        id: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+        name: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+        'country.name': { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+        'country.code': { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+        company: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+        representative: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+        balance: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+        date: { value: null, matchMode: FilterMatchMode.STARTS_WITH },
+    });
+    const [loading, setLoading] = useState(true);
+    const [globalFilterValue, setGlobalFilterValue] = useState('');
+
+    useEffect(() => {
+        setCustomers(data);
+        setLoading(false);
+    }, []);
+
+    const onGlobalFilterChange = (e) => {
+        const value = e.target.value;
+        let _filters = { ...filters };
+
+        _filters['global'].value = value;
+
+        setFilters(_filters);
+        setGlobalFilterValue(value);
+    };
+
+    const renderHeader = () => {
+        return (
+            <div className="flex justify-content-end">
+                <span className="p-input-icon-left">
+                    <i className="pi pi-search" />
+                    <InputText value={globalFilterValue} onChange={onGlobalFilterChange} placeholder="Keyword Search" />
+                </span>
+            </div>
+        );
+    };
+
+    const refresh =()=>{
+        window.location.reload();
+    }
+
+    const header = renderHeader();
+
+    const footer = (
+        <div className="flex justify-content-start">
+            <Button icon="pi pi-refresh" label="Reload" severity="warning" onClick={refresh}/>
+        </div>
+    );
+
+    return (
+        <div className="card">
+            {
+                value == "Screen1" ? <DataTable value={customers} paginator rows={5} dataKey="id" filters={filters} filterDisplay="row" loading={loading}
+                    globalFilterFields={['id', 'name', 'country.name', 'representative.name', 'status']} header={header} emptyMessage="No Data found.">
+                    <Column field="id" header="Id" filter filterPlaceholder="Search id" style={{ minWidth: '11rem' }} />
+                    <Column field="name" header="Name" filter filterPlaceholder="Search name" style={{ minWidth: '13rem' }} />
+                    <Column field="country.name" header="Country" filter filterPlaceholder="Search country" style={{ minWidth: '12rem' }}  />
+                    <Column field="company" header="Company" filter filterPlaceholder="Search company" style={{ minWidth: '12rem' }}  />
+                    <Column field="representative" header="Representative" filter filterPlaceholder="Search representative" style={{ minWidth: '13rem' }} />
+                    <Column field="balance" header="Balance" filter filterPlaceholder="Search balance" style={{ minWidth: '12rem' }} />
+                </DataTable>
+                    :
+                    <DataTable value={customers} footer={footer} paginator rows={5} dataKey="id" filters={filters} filterDisplay="row" loading={loading}
+                        emptyMessage="No Data found.">
+                        <Column field="id" header="Id" filter filterPlaceholder="Search by id" style={{ minWidth: '12rem' }} sortable />
+                        <Column field="country.name" header="Country" filter filterPlaceholder="Search by country" style={{ minWidth: '12rem' }} sortable />
+                        <Column field="country.code" header="Country Code" filter filterPlaceholder="Search by country code" style={{ minWidth: '12rem' }} sortable />
+                        <Column field="date" header="Date" filter filterPlaceholder="Search Date (YYYY-MM-DD)" style={{ minWidth: '12rem' }} sortable />
+                    </DataTable>
+            }
+        </div>
+    );
+}
+
+export default Screen
